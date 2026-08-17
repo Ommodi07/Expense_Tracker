d651c38eec73963b667e0f64ea62142fe24fd256
Ommodi07 <ommodi0207@gmail.com>
2026-08-17 13:15:53 +0530

    Update allowed hosts for development environment: add new IP address and enable debug mode

--- roommate_expenses/roommate_expenses/settings.py
+++ roommate_expenses/roommate_expenses/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-q9v*l8tm6b*sw5g2g4p*1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,174.129.131.243').split(',')
 
 # Security settings for production
 if not DEBUG:
